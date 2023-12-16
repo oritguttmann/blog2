f48f6613be2cb201cbc1458c41265c135f7e3ae9
oritguttmann <153425579+oritguttmann@users.noreply.github.com>
2023-12-16 21:12:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -141,8 +141,8 @@ def get_all_posts():
     global logged_in
     result = db.session.execute(db.select(BlogPost))
     posts = result.scalars().all()
-    print(logged_in)
-    print(current_user.id)
+    print(f"logged_in: {logged_in}")
+    print(f"current_user.id: {current_user.id}")
     if logged_in:
         curr_id = current_user.id
     else:
